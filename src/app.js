c97ed9255eba476a549dcc7bc7334bf15fb68039
Patricia Fopiani <patrifr2517@gmail.com>
2025-12-03 11:10:43 +0000

    carta

--- src/app.js
+++ src/app.js
@@ -7,5 +7,21 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector(".card").classList.add(GenerateRandomSuit());
+  document.querySelector(".card").innerHTML=GenerateRandomNumber();
 };
+
+const GenerateRandomNumber=()=>{
+  const numbers = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+  const indexNumbers = Math.floor(Math.random() * numbers.length);
+  return numbers[indexNumbers];
+}
+const GenerateRandomSuit=()=>{
+  const suit = ["diamond", "spade", "heart", "club"];
+  const indexSuit = Math.floor(Math.random() * suit.length);
+  return suit[indexSuit];
+}
+  
+
+
+  
